dfe5fe0a8a54205419139a41cefde5fe845e8f22
Dhruvil Lathiya <dhruvillathiya5858@gmail.com>
2022-11-17 19:29:08 +0530

    update CORS

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 const xss = require("xss-clean");
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(cors());
 app.use(xss());
 
